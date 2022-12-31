ca396abdae24fa7dbe51b0b7071ba2e3169124e5
Azkarel <azkarel63@gmail.com>
2022-12-31 16:28:39 +0100

    Empezamos a tener graficas

--- calcula.py
+++ calcula.py
@@ -7,6 +7,7 @@ Created on Thu Dec 29 12:42:25 2022
 """
 
 import tkinter as tk
+import matplotlib.pyplot as plt
 
 import bloque as bk
 
@@ -14,19 +15,38 @@ def calculaCalentamiento(regimen):
     
     flag = True
     iteracion = 1
+    x = list()
+    y1 = list()
+    y2 = list()
+    y3 = list()
+    y4 = list()
     
     while (flag and (iteracion<1440)):
     
         flag = False
-    
+        ysub1 = list()
+        ysub2 = list()
+        ysub3 = list()
+        ysub4 = list()
+   
         for bl in bk.bloques:
-        
+ 
             print(iteracion, end=", ")
+            
             print(bl.nInd, end=", ")
-            print(round((bl.temp - bl.tAmb), 2), end=", ")
+            
+            temperatureIncrement = bl.temp - bl.tAmb
+            print(round(temperatureIncrement, 2), end=", ")
+            ysub1.append(temperatureIncrement) 
+            
             print(round(bl.pk, 2), end=", ")
+            ysub2.append(bl.pk) 
+            
             print(round(bl.pConv, 2), end=", ")
+            ysub3.append(bl.pConv) 
+            
             print(round(bl.pRad, 2))
+            ysub4.append(bl.pRad) 
         
             bl.actualizaTemp(regimen, 60.0)
         
@@ -35,6 +55,31 @@ def calculaCalentamiento(regimen):
                 flag = True
     
         print("\n")
+        
+        x.append(iteracion)
+        y1.append(ysub1)
+        y2.append(ysub2)
+        y3.append(ysub3)
+        y4.append(ysub4)
 
         iteracion = iteracion + 1
 
+    fig, ax = plt.subplots()
+    ax.plot(x, y1)
+    ax.set_title('Incrementos de Temperatura')
+    plt.show()
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, y2)
+    ax.set_title('Perdidas generadas')
+    plt.show()
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, y3)
+    ax.set_title('Perdidas disitadas por conveccion')
+    plt.show()
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, y4)
+    ax.set_title('Perdidas disitadas por radiacion')
+    plt.show()
